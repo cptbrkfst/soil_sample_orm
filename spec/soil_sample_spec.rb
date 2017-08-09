cf850948bc7c097a4a1e7d6d23916347fc0f2b15
cptbrkfst <dyl@resonategroup.ca>
2017-08-09 19:07:50 -0400

    added first test

--- spec/soil_sample_spec.rb
+++ spec/soil_sample_spec.rb
@@ -16,4 +16,23 @@ describe Sample do
     DB[:conn].execute(sql)
     DB[:conn].results_as_hash = true
   end
+
+  let(:attributes) {
+    {
+      id:           nil,
+      name:         'test_sample',
+      description:  'test_sample_description',
+      location:     'test_sample_location'
+    }
+  }
+
+  let(:new_sample) { Sample.new(attributes) }
+
+  describe '.table_name' do
+    it 'creates a downcased, plural table name based on Class name' do
+      expect(Sample.table_name).to eq('samples')
+    end
+  end
+
+  
 end
