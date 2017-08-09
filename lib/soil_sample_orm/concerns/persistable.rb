91a4eb3fa78acf3784d3a24d7209268e7b137d56
cptbrkfst <dyl@resonategroup.ca>
2017-08-09 18:09:31 -0400

    added lifecycle events

--- lib/soil_sample_orm/concerns/persistable.rb
+++ lib/soil_sample_orm/concerns/persistable.rb
@@ -1,10 +1,21 @@
 module Persistable
   # Class methods for ORM persistence
   module ClassMethods
+    def self.extended(base) # Hook
+      puts "#{base} has been extended by #{self}"
+      base.attributes.keys.each do |attribute_name|
+        attr_accessor attribute_name
+      end
+    end
 
+    def attributes
+      self::ATTRIBUTES
+    end
   end
   # Instance methods for ORM persistence
   module InstanceMethods
-
+    def self.included(base) # Hook
+      puts "#{base} has mixed in #{self}"
+    end
   end
 end
