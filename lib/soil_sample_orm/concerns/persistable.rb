264d5171ba3c2d6687e198509b8f3d02fec84bb1
cptbrkfst <dyl@resonategroup.ca>
2017-08-10 13:55:04 -0400

    added initialization & accessor methods

--- lib/soil_sample_orm/concerns/persistable.rb
+++ lib/soil_sample_orm/concerns/persistable.rb
@@ -1,11 +1,22 @@
 module Persistable
+  DB = { :conn => SQLite3::Database.new('db/soil_sample.db') }.freeze
   # Class methods for ORM persistence
   module ClassMethods
     def self.extended(base) # Hook
       puts "#{base} has been extended by #{self}"
-      base.attributes.keys.each do |attribute_name|
-        attr_accessor attribute_name
+    end
+
+    def column_names
+      DB[:conn].results_as_hash = true
+
+      sql = "pragma table_info('#{table_name}')"
+
+      table_info = DB[:conn].execute(sql)
+      column_names = []
+      table_info.each do |row|
+        column_names << row['name']
       end
+      column_names.compact
     end
 
     def attributes
@@ -20,6 +31,12 @@ module Persistable
   module InstanceMethods
     def self.included(base) # Hook
       puts "#{base} has mixed in #{self}"
+    end
+
+    def initialize(options = {})
+      options.each do |key, value|
+        self.send("#{key}=", value)
+      end
     end
   end
 end
