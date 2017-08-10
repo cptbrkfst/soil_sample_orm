23abb385907e14b11d58ed52fd6770e027b5b923
cptbrkfst <dyl@resonategroup.ca>
2017-08-09 20:35:02 -0400

    changed filename

--- lib/soil_sample_orm/models/sample.rb
+++ lib/soil_sample_orm/models/sample.rb
@@ -1,11 +1,11 @@
 # Sample Object Class
 class Sample
   ATTRIBUTES = {
-    id: 'INTEGER PRIMARY KEY',
-    name: 'TEXT',
-    description: 'TEXT',
-    location: 'TEXT'
-  }.freeze
+    :id => 'INTEGER PRIMARY KEY',
+    :name => 'TEXT',
+    :description => 'TEXT',
+    :location => 'TEXT'
+  }
 
   include Persistable::InstanceMethods
   extend  Persistable::ClassMethods
