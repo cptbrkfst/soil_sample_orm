742a15af210059fe415242444b58e9de1e34a5b6
cptbrkfst <dyl@resonategroup.ca>
2017-08-09 20:35:23 -0400

    added tests

--- spec/soil_sample_spec.rb
+++ spec/soil_sample_spec.rb
@@ -26,13 +26,37 @@ describe Sample do
     }
   }
 
-  let(:new_sample) { Sample.new(attributes) }
+  let(:new_sample) { Sample.new }
+
+  describe '.attributes' do
+    it 'returns hash of class attributes' do
+      expect(Sample.attributes).to eq(
+        {
+          :id => "INTEGER PRIMARY KEY",
+          :name => "TEXT",
+          :description => "TEXT",
+          :location => "TEXT"
+        }
+      )
+    end
+  end
+
+  describe 'attr_accessor' do
+    it 'creates attr_accessors for each column name' do
+      old_name = new_sample.name
+      new_name = new_sample.name = 'new_test_sample'
+      old_description = new_sample.description
+      new_description = new_sample.description = 'new_test_sample_description'
+      expect(old_name).to eq('test_sample')
+      expect(new_name).to eq('new_test_sample')
+      expect(old_description).to eq('test_sample_description')
+      expect(new_description).to eq('new_test_sample_description')
+    end
+  end
 
   describe '.table_name' do
     it 'creates a downcased, plural table name based on Class name' do
       expect(Sample.table_name).to eq('samples')
     end
   end
-
-  
 end
