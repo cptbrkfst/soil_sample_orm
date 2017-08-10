c5f4d6a978741c65802afbdc5909c56c11453895
cptbrkfst <dyl@resonategroup.ca>
2017-08-10 13:55:43 -0400

    added dynamic accessor method

--- lib/soil_sample_orm/models/sample.rb
+++ lib/soil_sample_orm/models/sample.rb
@@ -1,12 +1,16 @@
 # Sample Object Class
 class Sample
   ATTRIBUTES = {
-    :id => 'INTEGER PRIMARY KEY',
-    :name => 'TEXT',
-    :description => 'TEXT',
-    :location => 'TEXT'
+    id: 'INTEGER PRIMARY KEY',
+    name: 'TEXT',
+    description: 'TEXT',
+    location: 'TEXT'
   }
 
   include Persistable::InstanceMethods
   extend  Persistable::ClassMethods
+
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
 end
