badad697a058b86101b6a650c17bbf9904a43a9b
cptbrkfst <dyl@resonategroup.ca>
2017-08-14 16:11:57 -0400

    added creation and insertion

--- lib/soil_sample_orm/concerns/persistable.rb
+++ lib/soil_sample_orm/concerns/persistable.rb
@@ -23,8 +23,31 @@ module Persistable
       self::ATTRIBUTES
     end
 
+    def attribute_names_for_insert
+      self.attributes.keys[1..-1].join(',')
+    end
+
+    def question_marks_for_insert
+      (self.attributes.keys.size - 1).times.collect { '?' }.join(',')
+    end
+
     def table_name
       "#{self.to_s.downcase}s"
+    end
+
+    def create_sql
+      self.attributes.collect do |attr_name, schema|
+        "#{attr_name} #{schema}"
+      end.join(',')
+    end
+
+    def create_table
+      sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS #{self.table_name} (
+          #{self.create_sql}
+        )
+      SQL
+      DB[:conn].execute(sql)
     end
   end
   # Instance methods for ORM persistence
@@ -37,6 +60,29 @@ module Persistable
       options.each do |key, value|
         self.send("#{key}=", value)
       end
+    end
+
+    def insert
+      sql = <<-SQL
+        INSERT INTO #{self.class.table_name} (#{self.class.attribute_names_for_insert})
+        VALUES (#{self.class.question_marks_for_insert})
+      SQL
+      DB[:conn].execute(sql, *attribute_values)
+      self.id = DB[:conn].execute('SELECT last_insert_rowid();')[0][0]
+    end
+
+    def attribute_values
+      self.class.attributes.keys[1..-1].collect do |attr_name|
+        self.send(attr_name)
+      end
+    end
+
+    def destroy
+      sql = <<-SQL
+        DELETE FROM #{self.class.table_name} WHERE id = ?
+      SQL
+
+      DB[:conn].execute(sql, self.id)
     end
   end
 end
